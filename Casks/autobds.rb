8552edbe642661f4ad1464484f72091399110512
Andy Huynh <vodka.coc.tm@gmail.com>
2025-10-31 16:45:18 +0700

    chore: bump to v0.2.6

--- Casks/autobds.rb
+++ Casks/autobds.rb
@@ -1,13 +1,13 @@
 cask "autobds" do
-  version "0.2.5"
+  version "0.2.6"
 
   # Architecture-specific downloads
   if Hardware::CPU.arm?
     url "https://pub-c378a27ce1204d59aadecec2b7e4f969.r2.dev/v#{version}/autoBDS-#{version}-arm64.dmg"
-    sha256 "8d4a82456805b1b25c154b4410059bb9479220b1c061248698d304d106da74cd"  # TODO: Update after uploading v0.2.1 DMG
+    sha256 "80ef2adba53a77c41762f938622062a458f3b5ca1ed24c51e1c8f5a57f0ee962"  # TODO: Update after uploading v0.2.1 DMG
   else
     url "https://pub-c378a27ce1204d59aadecec2b7e4f969.r2.dev/v#{version}/autoBDS-#{version}.dmg"
-    sha256 "458cdee5007a5b060c7e5f0808c6687baaa0796e93b9908b3c1380ced8bd37f0"  # TODO: Update after uploading v0.2.1 DMG
+    sha256 "558bd563e33fe43d26eb13a063759e54106259140c51e4353d095ec7778d71e4"  # TODO: Update after uploading v0.2.1 DMG
   end
 
   name "autoBDS"
